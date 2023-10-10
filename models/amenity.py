571e4f66077f8ebc013ea0960baf566af6b53fab
JoanWaithira <joanwaithira.jw@gmail.com>
2023-10-10 05:08:50 -0700

    Creating the amenity class

---
+++ models/amenity.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""This module defines the amenity class that inherits from BaseModels"""
+from models.base_model import BaseModel
+
+class Amenity(BaseModel):
+    """This class inherits from BaseModel"""
+    def __init__(self, *args, **kwargs)
+    """The instances of amenity"""
+    self.name = ""
