77ac47701f725fc7ea1314e926d98ee064419881
JoanWaithira <joanwaithira.jw@gmail.com>
2023-10-10 06:16:22 -0700

    Adding args to the user

--- models/user.py
+++ models/user.py
@@ -4,7 +4,16 @@
 from models.basemodel import BaseModel
 
 class User(BaseModel):
-    """The user class is inheriting from BaseModel"""
+    """
+    The user class is inheriting from BaseModel
+
+    Args:
+    email (str): The email address of the user
+    password(str): The password
+    first_name (str): The first name of the user
+    last_name (str): The last name of the user
+
+    """
     def __init__(*args, **kwargs)
     self.email = ""
     self.password = ""
