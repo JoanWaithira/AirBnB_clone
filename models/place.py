48e9c380a274eecd7d19cf10d74434252e6981c2
JoanWaithira <joanwaithira.jw@gmail.com>
2023-10-10 06:09:24 -0700

    Adding arguments in comments

--- models/place.py
+++ models/place.py
@@ -5,7 +5,21 @@ from models.base_model import BaseModel
 class Place(BaseModel):
     """ The place inherits from BaseModel"""
     def __init__(self, *args, **kwargs):
-        """The instances of place class """
+        """
+        Args:
+        city_id (str): The ID of the city located
+        user_id (str): The ID of the owner
+        name (str): The name of the place
+        description (str): Short description of the place
+        number_rooms (int): The number of rooms
+        number_bathrooms (int): The number of bathrooms
+        max_guest (int): The maximum number of guests allowed
+        price_by_night (int): The price per night
+        latitude (float): The coordinates
+        longitude (float): The coordinates
+        amenity_ids (list): The list of amenity IDs
+
+        """
         self.city_id = ""
         self.user_id = ""
         self.name = ""
--- models/review.py
+++ models/review.py
@@ -4,7 +4,14 @@
 class Review(BaseModel):
     """The review class inherits from BaseModel"""
     def __init__(self, *args, **kwargs):
-        """The instances of a review class"""
+        """
+        Args:
+        place_id (str): The ID of the place
+        user_id (str): The user ID of the person who has reviewed
+        text (str): The review message
+
+        """
+        super().__init__(*args, **kwargs)
         self.place_id = ""
         self.user_id = ""
         self.text = ""
