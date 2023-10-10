b1ab9f3e314ec65ac63818b37b5956825bfb9f37
JoanWaithira <joanwaithira.jw@gmail.com>
2023-10-10 05:33:35 -0700

    Modifying city.py

--- models/city.py
+++ models/city.py
@@ -6,6 +6,13 @@ from models.base_model import BaseModel
 class City(BaseModel):
     """ A class representing a city"""
     def __init__(self, *args, *kwargs):
-        """ Initialising instances of a city"""
+        """
+        Initialising instances of a city
+
+        Args:
+        state_id (str): The ID of the state 
+        name(str) : The name of the city
+
+        """
         state_id = ""
         name = ""
