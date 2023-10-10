4d29294cb483b4acafdfeff59e1fab367dee9a2c
JoanWaithira <joanwaithira.jw@gmail.com>
2023-10-10 05:04:49 -0700

    Creating a state class

--- models/state.py
+++ models/state.py
@@ -6,5 +6,5 @@ from model.base_model import BaseModel
 class State(BaseModel):
     """The State class inherits from the BaseModel"""
     def __init__(self, *args, **kwargs):
-        """Initialising an instance of the State"""
+        """Initialising an instance of the State class"""
         name = ""
