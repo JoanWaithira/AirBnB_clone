fbca2acd1f62814ec1e14017f977e5f3b89267b1
Gus-Victrix <iamfrsncis@gmail.com>
2023-10-10 05:06:00 +0300

    Added file_storage module with empty FileStorage class

---
+++ models/engine/file_storage.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+'''
+This module defines FileStorage which is used to serializes and deserializes
+instances to and fro json.
+'''
+
+import json
+
+class FileStorage:
+    pass
