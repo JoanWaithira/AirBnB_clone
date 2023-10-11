1547393ed10e2797df809103e4617bfa9759d1bc
Gus-Victrix <iamfrsncis@gmail.com>
2023-10-11 18:38:02 +0300

    Updates to console module

--- console.py
+++ console.py
@@ -17,6 +17,7 @@ from models.state import State
 class HBNBCommand(cmd.Cmd):
     ''' This class creates the project console and defines it's behaviors. '''
     prompt = '(hbnb)'
+    _classes = {"State", "User", "BaseModel", "Place", "Review", "Amenity"}
 
     def do_EOF(self, line=None):
         ''' Exits the console when EOF is encountered. '''
@@ -68,14 +69,138 @@ class HBNBCommand(cmd.Cmd):
         if not raw_string:
             print("** class name is missing **")
             return False
-        inputs = raw_string.split()
+        inputs = shlex.split(raw_string)
         if not inputs[1]:
             print("** instance id missing **")
             return False
-        if inputs[0] != "BaseModel":
+        if inputs[0] not in self._classes:
             print("** class doesn't exist **")
             return False
+        instance = f"{inputs[0]}.{inputs[1]}"
+        try:
+            print(models.storage.all()[instance])
+        except Exception:
+            print("** no instance found **")
+            return False
+        return
+
+    def do_destroy(self, raw_string):
+        '''
+        Delete an instance based on classname and id.
+
+        Usage: $ destroy <classname> <id>
+
+        Exceptions:
+            If class name is missing, ** class name missing ** is printed.
+            If class name doesn't exist, ** class doesn't exist ** is printed.
+            If id is missing, ** instance id missing ** is printed.
+            If class name/id pair doesn't exist, ** no instance found **
+                is printed.
+
+        Args:
+            raw_string (str):String containing classname, and id.
+        '''
+        if not raw_string:
+            print("** class name missing **")
+            return False
+        inputs = shlex.split(raw_string)
+        if len(inputs) == 1:
+            print("** instance id missing **")
+            return False
+        instance = f"{inputs[0]}.{inputs[1]}"
+        try:
+            del models.storage.all()[instance]
+        except Exception:
+            print("** no instance found **")
+            return False
+        return
+
+    def do_all(self, classname=None):
+        '''
+        Prints string representation of instances based or not on classname.
+        '''
+        instance_list = []
+        if not classname:
+            for instance in models.storage.all().values():
+                instance_list.append(instance.__str__())
+        else:
+            if classname not in self._classes:
+                print("** class doesn't exist **")
+                return False
+            for instance in models.storage.all().values():
+                if instance.__class__.__name__ == classname:
+                    instance_list.append(instance.__str__())
+        print(instance_list)
+        return
+
+    @staticmethod
+    def is_float(string):
+        '''
+        Checks if a string is a floating point number.
+
+        Args:
+            string (str): String to be tested.
+
+        Returns:
+            True if value is a float.
+            False if value is not a float.
+        '''
+        try:
+            float(string)
+            return True
+        except ValueError:
+            return False
+
+
+    def update(self, arg_string):
+        '''
+        Changes an attribute of an instance based on classname and id.
+        Usage: $ update <class name> <id> <attribute name> "<attribute values>"
+
+        Args:
+            arg_string (string): This contains args to be parsed.
+
+        Exceptions:
+            If class name is missing, ** class name missing ** is printed.
+            If class name doesn't exist, ** class doesn't exist ** is printed.
+            If id is missing, ** instance id missing ** is printed.
+            If class name/id pair doesn't exist, ** no instance found **
+                is printed.
+            If attribute name is missing, ** attribute name missing **.
+            If the value of the attributename doesn't exist, print
+                ** value missing **
+        '''
+        if not arg_string:
+            print("** class name missing **")
+            return False
+        arg_string = arg_string.replace(',', "")
+        inputs = shlex.split(arg_string)
         
+        if inputs[0] not in self._classes:
+            print("** class doesn't exist **")
+            return False
+        if len(inputs) == 1:
+            print("** instance id missing **")
+            return False
+        instance = f"{inputs[0]}.{inputs[1]}"
+        if instance not in models.storage.all():
+            print("** no instance found **")
+            return False
+        if len(inputs) == 2:
+            print("** attribute name missing **")
+            return False
+        if len(inputs) == 3:
+            print("** value missing **")
+            return False
+
+        if inputs[3].isnumeric():
+            inputs[3] = int(inputs[3])
+        elif is_float(inputs[3]):
+            inputs[3] = float(inputs[3])
+        setattr(models.storage.all()[instance], data[2], data[3])
+        return
+
+
 
 if __name__ == "__main__":
     HBNBCommand().cmdloop()
