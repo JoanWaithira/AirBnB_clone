83f6f9021ab605d4e3ac1ce324bb817255159971
JoanWaithira <joanwaithira.jw@gmail.com>
2023-10-14 12:08:51 -0700

    Making some changes

--- models/amenity.py
+++ models/amenity.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 """This module defines the amenity class that inherits from BaseModels"""
+
 from models.base_model import BaseModel
 
 
 class Amenity(BaseModel):
     """Class representing an amenity."""
-
     name = ""
 
     def __init__(self, *args, **kwargs):
