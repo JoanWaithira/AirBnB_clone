7ada98351ac7a1a41e956854c6c4d80afac140cb
JoanWaithira <joanwaithira.jw@gmail.com>
2023-10-14 13:28:30 -0700

    Adding in the bash line

--- models/__init__.py
+++ models/__init__.py
@@ -6,6 +6,5 @@ from models.engine.file_storage import FileStorage
 
 classes = {'BaseModel': 'BaseModel', 'Amenity': 'Amenity', 'State': 'State',
            'Place': 'Place', 'Review': 'Review', 'User': 'User'}
-
 storage = FileStorage()
 storage.reload()
