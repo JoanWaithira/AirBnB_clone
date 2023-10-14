3cf73a2bb1d885941ce5deb6b7f866d14da41ece
JoanWaithira <joanwaithira.jw@gmail.com>
2023-10-14 13:22:46 -0700

    Solving the errors

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,6 @@
 ''' This module initializes the FileStorage instance in the project. '''
-
 from models.engine.file_storage import FileStorage
+
 
 classes = {'BaseModel': 'BaseModel', 'Amenity': 'Amenity', 'State': 'State',
            'Place': 'Place', 'Review': 'Review', 'User': 'User'}
