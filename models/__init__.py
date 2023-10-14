0bd23f6c334bfd0a9ff3a3fdeebac85e9461ba28
JoanWaithira <joanwaithira.jw@gmail.com>
2023-10-14 14:46:31 -0700

    Changing __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -2,6 +2,8 @@
 ''' This module initializes the FileStorage instance in the project. '''
 from models.engine.file_storage import FileStorage
 
+classes = {'BaseModel': 'BaseModel', 'Amenity': 'Amenity', 'State': 'State',
+           'Place': 'Place', 'Review': 'Review', 'User': 'User'}
 
 storage = FileStorage()
 storage.reload()
