c9edd099603c9ed44b4692f9c22c67d8e66785d2
JoanWaithira <joanwaithira.jw@gmail.com>
2023-10-14 04:28:36 -0700

    Creating file storage tests

---
+++ tests/test_models/file_storage.py
@@ -0,0 +1,7 @@
+import unittest
+from models.base_model import BaseModel
+from models.engine.file_storage import FileStorage
+
+class TestFileStorage(unittest.TestCases):
+    """Test cases for File Storage"""
+
