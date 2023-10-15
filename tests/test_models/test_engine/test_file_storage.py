b35f8ea98b9fe7a090c10256f76e6a4d316c5e36
Gus-Victrix <iamfrsncis@gmail.com>
2023-10-15 14:44:59 +0300

    Moved old file_storage tests into new testing module.

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -59,3 +59,34 @@ class EngineStorageDocTest(unittest.TestCase):
             result.total_errors, 0, "Found code style errors (pycodestyle)."
         )
 
+
+class TestFileStorage(unittest.TestCase):
+    """Test cases for File Storage"""
+
+
+    def test_create_file_storage_instance(self):
+        """ Checks if an instance of the FileStorage class can be created."""
+        fs = FileStorage()
+        self.assertIsInstance(fs, FileStorage)
+
+    def test_file_storage_attributes(self):
+        """Checks the default attributes of the FileStorage class."""
+        fs = FileStorage()
+        self.assertEqual(fs._FileStorage__file_path, "file.json")
+        self.assertIsInstance(fs._FileStorage__objects, dict)
+
+    def test_all_method(self):
+        """Checks if the all method returns a dictionary."""
+        fs = FileStorage()
+        all_objects = fs.all()
+        self.assertIsInstance(all_objects, dict)
+
+    def test_new_method(self):
+        """Checks if the new method correctly stores an object"""
+        fs = FileStorage()
+        new_obj = BaseModel()
+        fs.new(new_obj)
+        self.assertIn(f"BaseModel.{new_obj.id}", fs._FileStorage__objects)
+
+if __name__ == "__main__":
+    unittest.main()
