38c4abe14b8eb22b11c304e32648fa469e133453
Gus-Victrix <iamfrsncis@gmail.com>
2023-10-15 15:26:01 +0300

    Added some more tests for test_file_storage.py

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -24,7 +24,7 @@ class EngineStorageDocTest(unittest.TestCase):
     Test the docs of the file engine
     """
 
-    def test_doc(self):
+    def test_docs(self):
         """
         Check all the doc of the Amenity Class
         """
@@ -63,6 +63,25 @@ class EngineStorageDocTest(unittest.TestCase):
 class TestFileStorage(unittest.TestCase):
     """Test cases for File Storage"""
 
+    def setUp(self):
+        '''Prepares the module for the tests.'''
+        try:
+            os.rename("file.json", "tmp.json")
+        except IOError:
+            pass
+        FileStorage._FileStorage__objects = {}
+
+    def tearDown(self):
+        '''Cleanup after testing.'''
+        try:
+            os.remove("file.json")
+        except IOError:
+            pass
+        try:
+            os.rename("tmp.json", "file.json")
+        except IOError:
+            pass
+        FileStorage._FileStorage__objects = {}
 
     def test_create_file_storage_instance(self):
         """ Checks if an instance of the FileStorage class can be created."""
@@ -72,8 +91,11 @@ class TestFileStorage(unittest.TestCase):
     def test_file_storage_attributes(self):
         """Checks the default attributes of the FileStorage class."""
         fs = FileStorage()
-        self.assertEqual(fs._FileStorage__file_path, "file.json")
+        self.assertIn("_FileStorage__objects", fs.__dict__)
         self.assertIsInstance(fs._FileStorage__objects, dict)
+        self.assertIn("_FileStorage__file_path", fs.__dict__)
+        self.assertIsInstance(fs._FileStorage__file_path, str)
+        self.assertEqual(fs._FileStorage__file_path, "file.json")
 
     def test_all_method(self):
         """Checks if the all method returns a dictionary."""
@@ -82,11 +104,75 @@ class TestFileStorage(unittest.TestCase):
         self.assertIsInstance(all_objects, dict)
 
     def test_new_method(self):
-        """Checks if the new method correctly stores an object"""
-        fs = FileStorage()
-        new_obj = BaseModel()
-        fs.new(new_obj)
-        self.assertIn(f"BaseModel.{new_obj.id}", fs._FileStorage__objects)
+        """checks if the new method correctly stores an object"""
+        newBaseModel = BaseModel()
+        newAmenity = Amenity()
+        newCity = City()
+        newReview = Review()
+        newState = State()
+        newUser = User()
+        for classname in ["BaseModel." + newBaseModel.id,
+                          "Amenity." + newAmenity.id,
+                          "Place." + newPlace.id,
+                          "City." + newCity.id,
+                          "Review." + newReview.id,
+                          "State." + newState.id,
+                          "User." + newUser.id]:
+            self.assertIn(classname, models.storage.all().keys())
+
+    def test_check_save(self):
+        """Check the saving of new instances into json file."""
+        newBaseModel = BaseModel()
+        newAmenity = Amenity()
+        newPlace = Place()
+        newCity = City()
+        newReview = Review()
+        newState = State()
+        newUser = User()
+        models.storage.save()
+        with open("file.json", 'r') as f:
+            buf = json.load(f)
+        for classname in ["BaseModel." + newBaseModel.id,
+                          "Amenity." + newAmenity.id,
+                          "Place." + newPlace.id,
+                          "City." + newCity.id,
+                          "Review." + newReview.id,
+                          "State." + newState.id,
+                          "User." + newUser.id]:
+            self.assertIn(classname, buf.keys())
+
+    def test_check_relaod(self):
+        """Check the reload method creation of new instances."""
+        newBaseModel = BaseModel()
+        newAmenity = Amenity()
+        newPlace = Place()
+        newCity = City()
+        newReview = Review()
+        newState = State()
+        newUser = User()
+        models.storage.save()
+        FileStorage._FileStorage__objects = {}
+        models.storage.reload()
+        for className in ["BaseModel." + newBaseModel.id,
+                          "Amenity." + newAmenity.id,
+                          "Place." + newPlace.id,
+                          "City." + newCity.id,
+                          "Review." + newReview.id,
+                          "State." + newState.id,
+                          "User." + newUser.id]:
+            self.assertIn(className, models.storage.all().keys())
+
+    def test_check_arguments(self):
+        '''Checking the Error output if arguments are used.'''
+        with self.assertRaises(AttributeError):
+            models.storage.new("Coucou")
+        with self.assertRaises(TypeError):
+            models.storage.save("Coucou")
+        with self.assertRaises(TypeError):
+            models.storage.all("Coucou")
+        with self.assertRaises(TypeError):
+            models.storage.reload("Coucou")
+
 
 if __name__ == "__main__":
     unittest.main()
