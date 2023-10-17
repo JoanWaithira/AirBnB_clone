711c00ee5587497b26c3c3c8fe0ffa5677e37742
Gus-Victrix <iamfrsncis@gmail.com>
2023-10-17 23:36:21 +0300

    Another attempt to solve the error.

--- models/city.py
+++ models/city.py
@@ -8,3 +8,7 @@ class City(BaseModel):
     """A class representing a city."""
     state_id = ""
     name = ""
+    def __init__(self, *args, **kwargs):
+        self.state_id = ""
+        self.name = ""
+        super().__init__(args, kwargs)
