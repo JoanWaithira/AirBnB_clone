faf2b2846ad70cfb9be716fed4f3724cfc18df23
Gus-Victrix <iamfrsncis@gmail.com>
2023-10-17 22:15:24 +0300

    Some changes to the test suite.

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,12 +1,109 @@
+#!/usr/bin/env python3
+
+
 import unittest
+from models import base_model
 from models.base_model import BaseModel
 from datetime import datetime
 import json
 import pycodestyle
+import os
 
 
 class TestBaseModel(unittest.TestCase):
     ''' Tests all features of BaseModel class. '''
+
+    def test_doc(self):
+        '''
+        Check all docs.
+        '''
+        # module documentation
+        module = len(base_model.__doc__)
+        self.assertGreater(module, 0)
+
+        # class documentation
+        module_class = len(BaseModel.__doc__)
+        self.assertGreater(module_class, 0)
+
+        module_class = len(BaseModel.__init__.__doc__)
+        self.assertGreater(module_class, 0)
+
+        module_class = len(BaseModel.__str__.__doc__)
+        self.assertGreater(module_class, 0)
+
+        module_class = len(BaseModel.save.__doc__)
+        self.assertGreater(module_class, 0)
+
+        module_class = len(BaseModel.to_dict.__doc__)
+        self.assertGreater(module_class, 0)
+
+    def test_pycodeStyle(self):
+        """Test that we conform to PEP-8."""
+        style = pycodestyle.StyleGuide(quiet=True)
+        result = style.check_files(["models/base_model.py"])
+        self.assertEqual(
+            result.total_errors, 0, "Found code style errors (pycodestyle)."
+        )
+
+    def setUp(self):
+        """setUp all instance we need"""
+        try:
+            os.rename("file.json", "tmp.json")
+        except IOError:
+            pass
+        self.my_model1 = BaseModel()
+        self.my_model2 = BaseModel()
+        self.my_model1.name = "My_First_Model"
+        self.my_model1.my_number = 89
+        self.my_model_json = self.my_model1.to_dict()
+
+    def tearDown(self):
+        """tearDown delete all instance"""
+        try:
+            os.remove("file.json")
+        except IOError:
+            pass
+        try:
+            os.rename("tmp.json", "file.json")
+        except IOError:
+            pass
+        del self.my_model1
+        del self.my_model2
+        del self.my_model_json
+
+    def test_idStr(self):
+        """test type of id"""
+        self.assertEqual(type(self.my_model1.id), str)
+
+    def test_uniqueId(self):
+        """test if id is unique"""
+        self.assertNotEqual(self.my_model1.id, self.my_model2.id)
+
+    def test_DateTimeCreated(self):
+        """test the date time"""
+        self.assertEqual(self.my_model1.created_at, self.my_model1.updated_at)
+        self.assertNotEqual(self.my_model1.created_at,
+                            self.my_model2.created_at)
+        self.assertNotEqual(self.my_model1.updated_at,
+                            self.my_model2.updated_at)
+        self.my_model2.save()
+        self.assertNotEqual(self.my_model2.created_at,
+                            self.my_model2.updated_at)
+
+    def test_strRepr(self):
+        """test the repr of class"""
+        strRep = self.my_model1.__str__()
+        self.assertIn(f"[BaseModel] ({self.my_model1.id})", strRep)
+        self.assertIn(f"'id': '{self.my_model1.id}'", strRep)
+        self.assertIn(
+            f"'created_at': {repr(self.my_model1.created_at)}", strRep)
+        self.assertIn(
+            f"'updated_at': {repr(self.my_model1.updated_at)}", strRep)
+
+    def test_to_dict_contains_added_attributes(self):
+        self.assertIn("name", self.my_model_json)
+        self.assertIn("my_number", self.my_model_json)
+
     def test_create_base_model_instance(self):
         """This tests that a new basemodel instance is set up"""
         self.model = BaseModel()
@@ -42,8 +139,8 @@ class TestBaseModel(unittest.TestCase):
     def test_str_representation(self):
         """Test the __str__ method of BaseModel."""
         model = BaseModel(id='test_id', name='Test Model')
-        expected_str = "[BaseModel] (test_id) {'id': 'test_id', 'name': 'Test Model'}"
-        self.assertEqual(str(model), expected_str)
+        expt = "[BaseModel] (test_id) {'id': 'test_id', 'name': 'Test Model'}"
+        self.assertEqual(str(model), expt)
         return
 
     def test_save_method(self):
